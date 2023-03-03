a34f2b8b351124cf6bc9e864be4d7d2370e56830
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-03-03 05:46:57 +0530

    add react-query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/homepage/Homepage';
+import Pokedex from './Pages/pokedex/Pokedex';
 import Start from './Pages/Start/Start';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/start" element={<Start />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
