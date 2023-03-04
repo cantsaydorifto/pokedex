e23503c1d97cb2b4fb472f1dc67ac16d8fb89654
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-03-05 02:02:46 +0530

    add framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Route, Routes } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/homepage/Homepage';
 import Pokedex from './Pages/pokedex/Pokedex';
@@ -7,15 +8,18 @@ import PokemonInfo from './Pages/pokemonInfo/PokemonInfo';
 import Start from './Pages/Start/Start';
 
 function App() {
+  const location = useLocation();
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/start" element={<Start />} />
-        <Route path="/pokedex" element={<Pokedex />} />
-        <Route path="/pokedex/:id" element={<PokemonInfo />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes key={location.pathname} location={location}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/start" element={<Start />} />
+          <Route path="/pokedex" element={<Pokedex />} />
+          <Route path="/pokedex/:id" element={<PokemonInfo />} />
+        </Routes>
+      </AnimatePresence>
     </QueryClientProvider>
   );
 }
